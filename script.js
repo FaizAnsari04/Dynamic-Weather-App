9d41a0cea39af5f513f3e4588b7d35ef0b37bf37
Faiz Ansari <135959497+FaizAnsari04@users.noreply.github.com>
2024-04-18 08:56:48 +0000

    main file

--- script.js
+++ script.js
@@ -1,47 +1,119 @@
-const container =document.querySelector('.container');
-const search =document.querySelector('.search-box button');
-const weatherBox=document.querySelector(',weather-box');
-const weatherDetails= document.querySelector('.weather-details');
-search.addEventListener('click',()=>
+
+/**const container = document.querySelector('.container');
+const Searchbtn = document.getElementById('Searchbtn');
+const weather = document.querySelector('.weather');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.desc');
+const humidity = document.getElementById('humidity');
+const windspeed= document.getElementById('windspeed');
+
+async function checkWeather(city)
 {
-    const APIKey='fa06af5e6cd8f3a18161c41aa37b5910';
-    const city= document.querySelector('.search-box input').ariaValueMax;
-    if (city=='')
-    return;
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units-metric&appid={APIKey}`).then(response => response.json()).then(json=>
-{
+    const api_key="fa06af5e6cd8f3a18161c41aa37b5910";
+    const api_url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data=await fetch(`${url}`).then(response=> response.json());
     
-const image= document.querySelector('.weather-box img');
-const temp= document.querySelector('.weather-box .temp');
-const desc= document.querySelector('.weather-box .desc');
-const humidity= document.querySelector('.weather-details .humidity span');
-const wind= document.querySelector('.weather-details .wind span');
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
 
-switch(json.weather[0].main)
-{
-        case 'Clear':
-           image.src="Images/Sunny.png";
-           break;
+    temp.innerHTML=`${Math.round(weather_data.main.temp-273-15)}C`;
+    desc.innerHTML=`${weather_data.weather[0].desc}`;
+    humidity.innerHTML=`${weather_data.main.humidity}%`;
+    windspeed.innerHTML=`${weather_data.main.windspeed} Km/H`;
 
-        case 'Cloud':
-            image.src="Images/Cloud.png";
-            break;
 
-        case 'Haze':
-            image.src="Images/mist.png";
-            break;
-        case 'Rain':
-            image.src="Images/rain.png";
-            break;
-
-        case 'Snow':
-            image.src="Images/Snow.png";
-        break;
-
-    default:
-        
 }
 
 
-});
+Searchbtn.addEventListener ('click',()=>{
+    checkWeather(container.value);
+
+})
+     switch(weather_data.weather[0].main)
+    {
+        case 'Clouds':
+            weather_img.src = "/Images/Cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/Images/Sunny.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/Images/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/Images/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/Images/Snow.png";
+            break;
+
+    }
+
+    console.log(weather_data);
+
+**/
+
+
+const container = document.querySelector('.container');
+const Searchbtn = document.getElementById('Searchbtn');
+const weather = document.querySelector('.weather');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.desc');
+const humidity = document.getElementById('humidity');
+const windspeed= document.getElementById('windspeed');
+const weather_img = document.querySelector('.weather-box img'); // Assuming this is where you want to display weather images
+
+async function checkWeather(city) {
+    const api_key = "fa06af5e6cd8f3a18161c41aa37b5910";
+    const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(api_url).then(response => response.json());
+
+    if (weather_data.cod === `404`) {
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        console.log("error");
+        return;
+    }
+
+    console.log("run");
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+
+    temp.innerHTML = `${Math.round(weather_data.main.temp - 273 - 15)}C`;
+    desc.innerHTML = `${weather_data.weather[0].desc}`;
+    humidity.innerHTML = `${weather_data.main.humidity}%`;
+    windspeed.innerHTML = `${weather_data.wind.speed} Km/H`; // Assuming this is where the wind speed is
+
+    switch (weather_data.weather[0].main) {
+        case 'Clouds':
+            weather_img.src = "/Images/Cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "/Images/Sunny.png";
+            break;
+        case 'Rain':
+            weather_img.src = "/Images/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "/Images/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "/Images/Snow.png";
+            break;
+    }
+
+    console.log(weather_data);
+}
+
+Searchbtn.addEventListener('click', () => {
+    checkWeather(container.value);
 });
